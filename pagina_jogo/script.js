70150847fac941bd745fcdcab4b9524c1c3e3c32
Mai-gomes <151780908+Mai-gomes@users.noreply.github.com>
2023-11-27 21:11:36 -0300

    Update script.js

--- pagina_jogo/script.js
+++ pagina_jogo/script.js
@@ -9,3 +9,19 @@ let voltarLog = document.querySelector("#btn-retorno");
 let btnRanking = document.querySelector("#botao-ranking");
 let crpTabela = document.querySelector("#corpo-tabela");
 let btnVoltarRank = document.querySelector("#btn-rank-voltar");
+
+let count = 0;
+
+let seqVitoria = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 4, 8],
+    [6, 4, 2],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
+
