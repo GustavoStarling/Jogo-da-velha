b5c5b91fcdcddcacf9551186ff47df7541e3a58d
Mai-gomes <151780908+Mai-gomes@users.noreply.github.com>
2023-11-27 21:28:08 -0300

    Update script.js

--- pagina_jogo/script.js
+++ pagina_jogo/script.js
@@ -52,3 +52,24 @@ else {
         nPartidas: 0
     },
 
+     let contasLidas;
+contasLidas = JSON.parse(localStorage.getItem("contas"));
+
+if (contasLidas != null) {
+    contas = contasLidas
+}
+else {
+    contas = [{
+        nome: "teste1",
+        senha: "qogbvoiadb",
+        nVitorias: 0, 
+        nPartidas: 0
+    },
+    {
+        nome: "teste2",
+        senha: "apsifnapvdkp",
+        nVitorias: 0, 
+        nPartidas: 0
+    }]
+}         
+
